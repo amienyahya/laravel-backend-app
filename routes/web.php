aaa50536fbd81b17c692bf456718f9a8bb1f346c
amienyahya <amien.yahya5@gmail.com>
2023-11-01 21:17:48 +0700

    Laravel Authorize_user role_update profil

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,12 @@ Route::get('/', function () {
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('home', function () {
-        return view('pages.blank-page', ['type_menu' => '']);
-    })->name('home');
+        return view('pages.dashboard', ['type_menu' => '']);
+    })->name('home')->middleware('can:dashboard');
+
+    Route::get('profile-edit', function () {
+        return view('pages.profile', ['type_menu' => '']);
+    })->name('profile.edit');
 });
 
 // Route::get('/login', function () {
